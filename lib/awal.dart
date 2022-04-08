894e47bdf0d5cbaf6e10710e62ae15e267a908e6
bayudilan <bayudila17@gmail.com>
2022-04-08 23:49:29 +0700

    jobdesk bayu

---
+++ lib/awal.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/daftar.dart';
+import 'package:flutter_application_1/login.dart';
+
+class awal extends StatefulWidget {
+  const awal({Key? key}) : super(key: key);
+
+  @override
+  State<awal> createState() => _awalState();
+}
+
+class _awalState extends State<awal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 0, 100, 0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 250,
+                  height: 100,
+                  decoration: BoxDecoration(),
+                  child: Align(
+                    alignment: AlignmentDirectional(25, 0.5),
+                    child: Text(
+                      'Selamat Datang di',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontFamily: "opensans",
+                        fontWeight: FontWeight.w900,
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  height: 100,
+                  decoration: BoxDecoration(),
+                  child: Text(
+                    'Alo ',
+                    style: TextStyle(
+                      fontFamily: 'Vidaloka',
+                      color: Colors.black,
+                      fontSize: 100,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  height: 100,
+                  decoration: BoxDecoration(),
+                  child: Text(
+                    'Vets',
+                    style: TextStyle(
+                      fontFamily: 'Vidaloka',
+                      color: Color.fromRGBO(110, 108, 200, 1),
+                      fontSize: 100,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsetsDirectional.fromSTEB(80, 275, 0, 0),
+                  width: 350,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(29),
+                    child: FlatButton(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                      color: Color(0xFF6E6CC8),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => login()));
+                      },
+                      child: Text(
+                        "Masuk",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0.8, 0),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
+                    child: GestureDetector(
+                      child: Text(
+                        'Tidak punya akun ? Daftar disini',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => daftar()));
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/awal.dart';
+import 'package:flutter_application_1/daftar.dart';
+
+class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  State<login> createState() => _awalState();
+}
+
+class _awalState extends State<login> {
+  bool _isObscure = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 25, 0, 0),
+                  child: Container(
+                    width: 350,
+                    height: 350,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFEEEEEE),
+                    ),
+                    child: Image.asset(
+                      'images/regis.jpg',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                  child: SizedBox(
+                    width: 250,
+                    child: Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          hintText: 'Email',
+                          contentPadding: EdgeInsets.all(15),
+                          prefixIcon: Icon(
+                            Icons.person,
+                          ),
+                        ),
+                        // Do something
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 25, 0, 0),
+                  child: SizedBox(
+                    width: 250,
+                    child: Center(
+                      child: TextField(
+                        obscureText: _isObscure,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            hintText: 'Password',
+                            prefixIcon: Icon(
+                              Icons.lock,
+                            ),
+                            suffixIcon: IconButton(
+                                icon: Icon(_isObscure
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () {
+                                  setState(() {
+                                    _isObscure = !_isObscure;
+                                  });
+                                })),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsetsDirectional.fromSTEB(100, 50, 100, 0),
+                  width: 450,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: FlatButton(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                      color: Color(0xFF6E6CC8),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => awal()));
+                      },
+                      child: Text(
+                        "Masuk",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0, 0),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
+                    child: GestureDetector(
+                      child: Text(
+                        'Tidak punya akun ? Daftar disini',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => daftar()));
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
